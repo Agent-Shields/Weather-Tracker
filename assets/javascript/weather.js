8a6757f61b4ccc0f8da478138494a124c93f0757
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-01-06 19:16:59 -0600

    remove the current / forecast comments

---
+++ assets/javascript/weather.js
@@ -0,0 +1,12 @@
+// key : 8b2ff7d80fb33fb5fa7171ccd4d16620
+
+// current weather http://bulk.openweathermap.org/snapshot/{BULK_FILE_NAME}?appid={API key}
+
+// forecast weather http://bulk.openweathermap.org/archive/{BULK_FILE_NAME}?appid={API key}
+
+
+var getWeather = function() {
+    // fetch("api.openweathermap.org/data/2.5/weather?q=blaine,MN&appid=8b2ff7d80fb33fb5fa7171ccd4d16620")
+}
+
+console.log(getWeather)
