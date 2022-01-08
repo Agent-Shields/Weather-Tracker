c284436e90d735542c8ff87ba10bdd0133f83fe0
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-01-08 15:12:35 -0600

    basic click event added

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,5 +1,18 @@
 // declaree HTML elements as vars
 
+var citySearched = document.querySelector("#city")
+console.log(citySearched);
+
+var searchButton = document.querySelector("#btnSearch")
+console.log(searchButton);
+
+// Add event for search click
+
+searchButton.addEventListener("click", function(){
+    console.log("clicked search");
+})
+
+
 // var getWeather = function() {
     
 // }
