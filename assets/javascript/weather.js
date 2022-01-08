b6d84e0a0465d5e72b3ec4028eff7a73cf1fb011
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-01-08 14:53:47 -0600

    initial commit

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,12 +1,7 @@
-// key : 8b2ff7d80fb33fb5fa7171ccd4d16620
+// declaree HTML elements as vars
 
-// current weather http://bulk.openweathermap.org/snapshot/{BULK_FILE_NAME}?appid={API key}
-
-// forecast weather http://bulk.openweathermap.org/archive/{BULK_FILE_NAME}?appid={API key}
-
-
-var getWeather = function() {
+// var getWeather = function() {
     
-}
+// }
 
-console.log(getWeather)
+// console.log(getWeather)
