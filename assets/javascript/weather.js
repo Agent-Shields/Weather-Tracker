075e15a6df5638ff6a6bff97dca49e6c539ea46e
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-01-08 16:56:56 -0600

    added search history functionality

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,11 +1,12 @@
 // declaree HTML elements as vars
 
-var citySearched = document.querySelector("#city")
+var citySearched = document.querySelector("#city");
 
+var searchButton = document.querySelector("#btnSearch");
 
-var searchButton = document.querySelector("#btnSearch")
+var searchHistoryList = document.querySelector("#searchList")
 
-// define click handler 
+// define button click handler 
 
 var buttonClickHandler = function(event) {
     // prevent page from refreshing
@@ -15,16 +16,27 @@ var buttonClickHandler = function(event) {
     var cityName = citySearched.value.trim();
    
     if (cityName) {
-        console.log(cityName)
-        localStorage.setItem("cityNamed", cityName)
+        console.log(cityName);
+        localStorage.setItem("cityNamed", cityName);
+
+        // clear searched city from input field
+        citySearched.value = "";
+        citySearched.focus();
         // get lat-lon of city named
     } else {
-        alert("Please enter a city name in the search input field")
+        alert("Please enter a city name in the search input field");
     }
+
+    // add cityName to search history 
+    var searchHistoryItem = document.createElement("button");
+    searchHistoryItem.classList.add("btn", "btn-secondary");
+    searchHistoryItem.textContent = cityName;
+    searchHistoryList.appendChild(searchHistoryItem)
+
 }
                                                                                                                                                                                                                                         
 // Add event listeners for search submit
 
-searchButton.addEventListener("click", buttonClickHandler)
+searchButton.addEventListener("click", buttonClickHandler);
 
 
